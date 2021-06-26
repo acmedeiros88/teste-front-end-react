070750d7fcff04db45efbab5937b97b24122821b
Adriano Medeiros <adriano_cardoso@hotmail.com.br>
2021-06-26 00:08:19 -0300

    Projeto Front-End com React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import Table from 'react-bootstrap/Table'
+import api from "./api";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    produtos: []
+  };
+
+  async componentDidMount() {
+    const response = await api.get("/produtos/ativo?sort=quantidade");
+    this.setState({produtos: response.data});
+  };
+
+  render(){
+    const {produtos} = this.state; 
+    return(
+      <div>
+        <h1>PRODUTOS</h1>
+        <Table striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>Nome do Produto</th>
+              <th>Quantidade</th>
+              <th>ativo</th>
+            </tr>
+          </thead>
+          <tbody>
+          {produtos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.nome_produto}</td>
+              <td>{produto.quantidade}</td>
+              <td>{produto.ativo.toString()}</td>
+            </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    );
+  };
+};
 
 export default App;
